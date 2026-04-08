2a2dd9c3522ea5c1feed6f1f95864ae0edab1828
ngkulov22 <ngkulov22@codingburgas.bg>
2026-04-08 23:53:17 +0300

    Finish login/register authorization

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,12 @@
 from flask import render_template
+from flask_login import login_required, current_user
 from . import main_bp
 
 @main_bp.route("/")
 def index():
     return render_template("main/index.html")
+
+@main_bp.route("/index")
+@login_required
+def dashboard():
+    return render_template("data/index.html", user=current_user)
--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from app import db, login_manager
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import login_manager
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
